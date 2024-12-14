4d070cf4dbe40f9036c8ea68d42a3749041c9949
Tatiana Shepeleva <tati.shep@gmail.com>
2024-12-14 11:58:18 +0800

    feat: add colors for both themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 import starlightPlugin from "@astrojs/starlight-tailwind";
-import colors from 'tailwindcss/colors';
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        accent: colors.indigo,
-        gray: colors.zinc,
+        primary: {
+          light: "#ffffff",
+          dark: "#111827",
+        },
+        text: {
+          light: "#111827",
+          dark: "#ffffff",
+        },
       },
     },
   },
